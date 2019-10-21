46a3308eca93842e8b1f105257509f2efcd43237
Byteme8bit <lynchpcconsulting@gmail.com>
2019-10-20 20:30:36 -0700

    dice_roll_simulation.py > Second iteration >> Added comments for all items >> Removed "game_continue" variable. Seemed redundant when can just call "while True" >> Added option to Quit in "after_results" >> BUG #1: Tested and program runs it loops and ignores the input given on "after_results"
    > Pending:
    >> Add input verification for variables: dice, total_sides, after_results
    >> Test for possible types of errors
    >> Add error handling with try / except
    >> Performance test large simulations?
    >> Cleanup / Minify / Make more Pythonic

--- dice_roll_simulation.py
+++ dice_roll_simulation.py
@@ -5,31 +5,40 @@ __author__ = "byteme8bit"
 # Module imports
 import random
 
+# Total number of dice
 dice = input("How many dice would you like to roll?:\n")
+
+# Total number of sides for dice
 total_sides = input("How many sides should each dice have?:\n")
-game_continue = True
 
-while game_continue:
+# Main loop
+while True:
 
+    # Empty list to contain results
     results = []
 
+    # Loops to simulate each dice rolling
     for i in range(int(dice)):
+
+        # Appends a randomly chosen integer to the results list
         results.append(random.randint(1, int(total_sides)))
 
+    # Displays the "dice rolls"
     print(*results)
 
+    # Asks user if program should repeat same parameters or use new ones
     after_results = input("Would you like to repeat this simulation or change the parameters?:\n"
-                          "[R]epeat   |   [C]hange")
+                          "[R]epeat   |   [C]hange   |   [Q]uit")
 
+    # If user chooses to repeat
     if after_results == 'R' or 'r':
         continue
 
+    # If user chooses to quit program
+    elif after_results == 'Q' or 'q':
+        quit(8)
+
+    # If user chooses to change parameters
     else:
         dice = input("How many dice would you like to roll?:\n")
         total_sides = input("How many sides should each dice have?:\n")
-        game_continue = True
-
-
-
-
-
