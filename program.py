f34b29b4b442a010d5d5490fe882a7fe0a28e4de
Byteme8bit <lynchpcconsulting@gmail.com>
2019-10-28 20:13:57 -0700

    program.py > Several (unpublished) Iterations Later >> Documented code further >> Tweaked code layout moving some UI and general logic items around. >>> The Welcome print statement for example was moved into __init__ as I felt that made logical sense to greet the user during the initialization >>> Moved after_results logic from inside roll_dice() method to part of the "program" code outside of the class entirely. Still internal debate going on over this move. >> Added simulation_to_file() method to cleanly store the session's simulations and max values to a text file >> Minimally imports from datetime in order to dynamically assign unique session_id and filenames. >> Tested briefly, further testing needed
    > Pending:
    >> Performance test large simulations?
    >> Cleanup / Minify / Make more Pythonic
    >> Adjust / Tweak / TTB (Try-to-Break)

--- program.py
+++ program.py
@@ -4,18 +4,29 @@ __author__ = "byteme8bit"
 
 # Module imports
 from random import randint
+from datetime import datetime
 
 
 class ByteDyce:
+    """
+    The ByteDyce class contains parameters to store various information for the current and past simulations.
+    """
 
     def __init__(self):
-        self.simulations_performed = 0
-        self.results = {}
-        self.max_dice = 0
-        self.max_sides = 0
-        self.dice = 0
-        self.total_sides = 0
-        self.current_sim = []
+        print("\nWelcome to the ByteDyce Simulation.\n"
+              "This program allows you to simulate dice rolls for whatever your needs may be. DnD, Board games, etc.\n")
+        self.session_id = datetime.now().strftime("%d-%m-%Y @ %H-%M-%S")
+        self.simulations_performed = 0  # Counts number of simulations performed in current session
+        self.results = {}  # Stores simulation results. Key = simulation #, Value = results
+        self.max_dice = 0  # Stores the highest number of dice simulated in current session
+        self.max_sides = 0  # Stores the highest number of dice sides simulation in current session
+        self.dice = 0  # Stores the current number of dice
+        self.total_sides = 0  # Stores the current number of dice sides
+        self.current_sim = []  # Stores the results of current simulation
+
+    # def __del__(self):
+    #     self.simulation_to_file()
+    #     quit(0)
 
     def find_max_dice(self, new, current):  # Stores highest number of dice simulated
         self.max_dice = new if new > current else current
@@ -23,88 +34,102 @@ class ByteDyce:
     def find_max_sides(self, new, current):  # Stores highest number of sides of dice simulated
         self.max_sides = new if new > current else current
 
-    def store_results(self, results):  # Stores results from each simulation
+    def store_results(self, results):  # Stores results from each simulation then increases counter by 1
         self.results[self.simulations_performed] = results, self.max_dice, self.max_sides
         self.simulations_performed += 1
 
     def print_results(self):
         print(f"\nHighest number of dice rolled: {self.max_dice}\n"
               f"Highest number of sides per dice: {self.max_sides}\n")
+
         for key in self.results.keys():
-            print(f"Simulation #{key}:\n"
+            print(f"Simulation #{key + 1}:\n"
                   f"Results: {self.results[key][0]}\n"
                   f"Dice rolled: {self.results[key][1]}\n"
                   f"Sides per dice: {self.results[key][2]}")
+        print()
 
     def get_dice_input(self):
         # Total number of dice
-        d = input("\nHow many dice would you like to roll?:\n")
+        dice = input("\nHow many dice would you like to roll?:\n")
 
         # Input verification to ensure user provides a number
-        while not d.isnumeric():
+        while not dice.isnumeric():
             print("\nPlease input a digit (e.g. 1, 3, 574, etc.)")
-            d = input("\nHow many dice would you like to roll?:\n")
+            dice = input("\nHow many dice would you like to roll?:\n")
 
-        self.dice = int(d)
+        self.dice = int(dice)
 
         self.find_max_dice(self.dice, self.max_dice)
 
         # Total number of sides for dice
-        t = input("\nHow many sides should each dice have?:\n")
+        sides = input("\nHow many sides should each dice have?:\n")
 
         # Input verification to ensure user provides a number
-        while not t.isnumeric():
+        while not sides.isnumeric():
             print("\nPlease input a digit (e.g. 1, 3, 574, etc.)")
-            t = input("\nHow many sides should each dice have?:\n")
+            sides = input("\nHow many sides should each dice have?:\n")
 
-        self.total_sides = int(t)
+        self.total_sides = int(sides)
 
         self.find_max_sides(self.total_sides, self.max_sides)
 
     def roll_dice(self):
+        # Creates list by looping "dice" times and selecting random numbers in range 1 thru "total_sides"
+        self.current_sim = [randint(1, int(self.total_sides)) for i in range(int(self.dice))]
 
-        # self.dice, self.total_sides = self.get_dice_input()
+        # Splats list to UI and stores simulation results
+        print("\n", *self.current_sim, "\n")
+        self.store_results(self.current_sim)
 
-        # Main loop
-        while True:
+    def simulation_to_file(self):
+        filename = ".\\" + self.session_id + ".txt"
+        file = open(filename, "w")
+        file.write(f"ByteDyce Simulation performed {self.session_id}\n"
+                   f"Number of simulations performed: {self.simulations_performed}\n"
+                   f"Max Dyce Rolled: \t{self.max_dice}\n"
+                   f"Max Dyce Sides: \t{self.total_sides}\n\n")
 
-            # Creates list by looping "dice" times and selecting random numbers in range 1 thru "total_sides"
-            self.current_sim = [randint(1, int(self.total_sides)) for i in range(int(self.dice))]
-
-            # Splats list
-            print("\n", *self.current_sim, "\n")
-            self.store_results(self.current_sim)
-
-            # Asks user if program should repeat same parameters or use new ones
-            after_results = input("\nWould you like to repeat this simulation or change the parameters?:\n"
-                                  "[R]epeat   |   [C]hange    |   [P]rint Results   |   [Q]uit\n").lower()
-
-            # Input verification to ensure user provides a valid response
-            while after_results not in "rcpq" or len(after_results) > 1:
-                print("\nPlease select valid answer r - c - p - q")
-                after_results = input("\nWould you like to repeat this simulation or change the parameters?:\n"
-                                      "[R]epeat   |   [C]hange   |   [P]rint Results   |   [Q]uit\n").lower()
-
-            # If user chooses to change parameters
-            if after_results == 'c':
-                self.get_dice_input()
-
-            # If user chooses to repeat
-            elif after_results == 'r':
-                pass
-
-            elif after_results == 'p':
-                self.print_results()
-
-            # If user chooses to quit program
-            else:
-                quit(8)
+        for key in self.results.keys():
+            file.write(f"Simulation #{key}:\n"
+                       f"Results: {self.results[key][0]}\n"
+                       f"Dice rolled: {self.results[key][1]}\n"
+                       f"Sides per dice: {self.results[key][2]}\n\n")
+        file.close()
 
 
 program = ByteDyce()
 
-print("\nWelcome to the ByteDyce Simulation.\n"
-      "This program allows you to simulate dice rolls for whatever your needs may be. DnD, Board games, etc.\n")
-
 program.get_dice_input()
-program.roll_dice()
+
+# Main loop
+while True:
+
+    program.roll_dice()
+
+    # Asks user if program should repeat same parameters or use new ones
+    after_results = input("\nWould you like to repeat this simulation or change the parameters?:\n"
+                          "[R]epeat   |   [C]hange    |   [P]rint Results   |   [Q]uit\n").lower()
+
+    # Input verification to ensure user provides a valid response
+    while after_results not in "rcpq" or len(after_results) > 1:
+        print("\nPlease select valid answer r - c - p - q")
+        after_results = input("\nWould you like to repeat this simulation or change the parameters?:\n"
+                              "[R]epeat   |   [C]hange   |   [P]rint Results   |   [Q]uit\n").lower()
+
+    # If user chooses to change parameters
+    if after_results == 'c':
+        program.get_dice_input()
+
+    # If user chooses to repeat
+    elif after_results == 'r':
+        pass
+
+    # If user chooses to print stored results of past simulations
+    elif after_results == 'p':
+        program.print_results()
+
+    # If user chooses to quit program
+    else:
+        program.simulation_to_file()
+        quit(8)
